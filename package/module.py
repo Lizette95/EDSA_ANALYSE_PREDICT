4b272e485401cbc751af7f9bc0b7fb47853945b9
Lizette Loubser <lizette.loubser@hotmail.com>
2020-02-07 08:50:43 +0200

    Modified function 4

--- package/module.py
+++ package/module.py
@@ -49,26 +49,19 @@ def dictionary_of_metrics(items):
 def extract_municipality_hashtags(df):
     """
     This function takes in a pandas dataframe of tweets and
-    extracts municipalities from a dictionary of municipalities.
+    extracts municipalities using a dictionary of municipalities.
     Hashtags are also extracted and returned as a list.
     Municipalities and hashtags are added as new columns and
     the modified dataframe is returned.
     """
     new_data = df
-    municipality_dict = { '@CityofCTAlerts' : 'Cape Town',
-            '@CityPowerJhb' : 'Johannesburg',
-            '@eThekwiniM' : 'eThekwini' ,
-            '@EMMInfo' : 'Ekurhuleni',
-            '@centlecutility' : 'Mangaung',
-            '@NMBmunicipality' : 'Nelson Mandela Bay',
-            '@CityTshwane' : 'Tshwane'}
     municipalities_list = []
     hashtags_list = []
     for row in new_data['Tweets']:
         row = row.replace(":","")
         #Extract municipalities
         municipalities = []
-        [municipalities.append(municipality_dict[word]) for word in row.split() if word in municipality_dict]
+        [municipalities.append(mun_dict[word]) for word in row.split() if word in mun_dict]
         municipalities_list.append(municipalities)
         #Extract hashtags
         hashtags = []
@@ -76,11 +69,11 @@ def extract_municipality_hashtags(df):
         hashtags_list.append(hashtags)
     #Add municipality column
     new_data['municipality'] = municipalities_list
-    new_data['municipality'] = new_data['municipality'].apply(lambda entry: np.nan if (type(entry) == list and len(entry) == 0) else entry)
-    new_data['municipality'] = new_data['municipality'].apply(lambda entry: ''.join(entry) if (type(entry) == list and len(entry) == 1) else entry)
+    new_data['municipality'] = new_data['municipality'].apply(lambda entry: ''.join(entry) if (type(entry) == list and len(entry) != 0) else np.nan)
     #Add hashtags column
     new_data['hashtags'] = hashtags_list
     new_data['hashtags'] = new_data['hashtags'].apply(lambda entry: np.nan if (type(entry) == list and len(entry) == 0) else entry)
+    #Return new dataframe
     return new_data
 
 ### FUNCTION 5: Number Of Tweets Per Day ###
