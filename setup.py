96f2aa48d2a6d50066a52f76c1db408afa77b1a3
Lizette Loubser <lizette.loubser@hotmail.com>
2020-02-16 15:55:31 +0200

    Renamed package folder & added .gitignore file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='A package for calculating metrics and analysing Twitter data',
+    description='A package for calculating summary metrics and analysing Twitter data',
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas'],
     url='https://github.com/MENZI-MCHUNU/EDSA_ANALYSE_PREDICT.git',
