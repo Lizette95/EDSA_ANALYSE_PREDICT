6f5956dce65e72958ce103c665d77cc9f81db288
Lizette Loubser <lizette.loubser@hotmail.com>
2020-02-18 13:16:51 +0200

    final push

--- tests/test.py
+++ tests/test.py
@@ -9,7 +9,6 @@ for col, row in ebp_df.iloc[:,1:].iteritems():
     ebp_df[col] = ebp_df[col].str.replace(',','').astype(int)
 twitter_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/twitter_nov_2019.csv'
 twitter_df = pd.read_csv(twitter_url)
-twitter_df.head()
 # gauteng ebp data as a list
 gauteng = ebp_df['Gauteng'].astype(float).to_list()
 # dates for twitter tweets
